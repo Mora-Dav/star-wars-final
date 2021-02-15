38be97b99efecf6b138ac53b9fc45de8dc3a6cc9
Mora-Dav <moradavi@gmail.com>
2021-02-15 10:28:59 -0300

    Creacion de ruteo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+}
+from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header"> 
+        <Router>
+        <nav>
+          <ul>
+            <li>
+            <Link to="/">Home</Link>
+            </li>
+            <li>
+            <Link to="/characters">Personajes</Link>
+            </li>
+            <li>
+            <Link to="/character/:id">Detalle</Link>
+            </li>
+          </ul>
+          </nav>
+        </Router>
+
+        <switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/characters">
+            <Personajes />
+          </Route>
+
+          <Route path="/character/:id">
+            <Personaje />
+          </Route>
+        </switch>
       </header>
+      <h1 className="title"> Star Wars  </h1>
+
+      
+
     </div>
   );
 }
